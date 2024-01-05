eebceee930eeb8a27d216eeda00e88dfda255ced
Samyak Patni <samyak.patni@ninebit.in>
2024-01-05 17:38:21 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Row } from 'react-bootstrap';
+
+function App() {
+  return (
+   <>
+   <div className='row'>
+      <div className='col-md-6'>ufhsdusu</div>
+      <div className='col-md-6'>
+        <img src="sideImg.png" alt="img" height="100%" width="100%"/>
+      </div>
+   </div>
+   </>
+  );
+}
+
+export default App;
