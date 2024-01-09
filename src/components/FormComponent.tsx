38d55865725a0228d9c365ad0af5e16be0cd44cc
Samyak Patni <samyak.patni@ninebit.in>
2024-01-09 17:38:40 +0530

    test running

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,9 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
 import { FcGoogle } from 'react-icons/fc';
 import axios from 'axios';
 import {
