1b63ed52524174e64f2aa87c90cf085b7ac4cba9
Samyak Patni <samyak.patni@ninebit.in>
2024-01-09 17:31:41 +0530

    testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Heading from './components/Heading';
+import { emailPlaceholder, mainHeading, passwordPlaceholder, rememberMeCheck, signInBtn, signInWithGoogleBtn, subHeading } from './strings';
+import FormComponent from './components/FormComponent';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+//Check For Image
+describe('The image component', () => {
+  test('alt contains correct value', () => {
+    render(<App/>)
+    const testImage = document.querySelector("img") as HTMLImageElement;
+    expect(testImage.alt).toContain("img");
+  })
+
+  // test('src contains correct value', () => {
+  //   render(<App/>)
+  //   const testImage = document.querySelector("img") as HTMLImageElement;
+  //   expect(testImage.alt).toContain("sideImg.png");
+  // })
 });
 
+
+//Heading Elements
+describe('Heading Component', () => {
+  test('renders text in heading', () => {
+    render(<Heading/>);
+
+    expect(screen.getByText(mainHeading)).toBeInTheDocument();
+    expect(screen.getByText(subHeading)).toBeInTheDocument();
+   
+  });
+})
+
+
+//Form Elements
+describe('Form Component', () => {
+
+  // Check for rendering
+  test('renders buttons and inputs',()=>{
+    render(<FormComponent/>);
+
+    expect(screen.getByRole('button', { name: signInBtn })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: signInWithGoogleBtn })).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(emailPlaceholder)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(passwordPlaceholder)).toBeInTheDocument();
+    expect(screen.getByLabelText(rememberMeCheck)).toBeInTheDocument();
+
+  })
+
+  //Check for updation of input fields on change
+//   test('updates fields on input change'),()=>{
+//     render(<FormComponent/>);
+
+
+//   }
+// })
+
+
+// test('updates fields on input change')
+
+// test('calls API on form submission')
+
+// test('handles API error on form submission')
+})
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           </div>
         </div>
         {/* Image Section */}
-        <div className='col-md-6' style={{height:"100vh"}}>
+        <div className='col-md-6'>
           <img src="sideImg.png" alt="img" height="100%" width="100%" />
         </div>
       </div>
